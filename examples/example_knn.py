092fc7a12ac64ac7c37a5d1bd50d8da3fc235fcc
Felix Igelbrink <felix.igelbrink@uni-osnabrueck.de>
2022-05-25 17:13:02 +0200

    Fixed windows build

---
+++ examples/example_knn.py
@@ -0,0 +1,12 @@
+import numpy as np
+from numba_kdtree import KDTree
+
+
+data = np.random.random(3_000_000).reshape(-1, 3).astype(np.float32)
+kdtree = KDTree(data, leafsize=10)
+
+# query the nearest neighbors of the first 100 points
+distances, indices = kdtree.query(data[:100], k=30)
+
+# query all points in a radius around the first 100 points
+indices = kdtree.query_radius(data[:100], r=0.5, return_sorted=True)
--- src/ckdtree/ckdtree_decl.h
+++ src/ckdtree/ckdtree_decl.h
@@ -6,30 +6,30 @@
 //#include <numpy/npy_common.h>
 #include <vector>
 #include <cmath>
+#include <sys/types.h>
  // some useful macros
 #ifdef HAVE___BUILTIN_EXPECT
-#define CKDTREE_LIKELY(x) __builtin_expect(!!(x), 1)
-#define CKDTREE_UNLIKELY(x) __builtin_expect(!!(x), 0)
+    #define CKDTREE_LIKELY(x) __builtin_expect(!!(x), 1)
+    #define CKDTREE_UNLIKELY(x) __builtin_expect(!!(x), 0)
 #else
-#define CKDTREE_LIKELY(x) (x)
-#define CKDTREE_UNLIKELY(x) (x)
+    #define CKDTREE_LIKELY(x) (x)
+    #define CKDTREE_UNLIKELY(x) (x)
 #endif
 
 #ifdef HAVE___BUILTIN_PREFETCH
-/* unlike _mm_prefetch also works on non-x86 */
-#define CKDTREE_PREFETCH(x, rw, loc) __builtin_prefetch((x), (rw), (loc))
+    /* unlike _mm_prefetch also works on non-x86 */
+    #define CKDTREE_PREFETCH(x, rw, loc) __builtin_prefetch((x), (rw), (loc))
 #else
-#ifdef HAVE__MM_PREFETCH
-/* _MM_HINT_ET[01] (rw = 1) unsupported, only available in gcc >= 4.9 */
-#define CKDTREE_PREFETCH(x, rw, loc) _mm_prefetch((x), loc == 0 ? _MM_HINT_NTA : \
-                                             (loc == 1 ? _MM_HINT_T2 : \
-                                              (loc == 2 ? _MM_HINT_T1 : \
-                                               (loc == 3 ? _MM_HINT_T0 : -1))))
-#else
-#define CKDTREE_PREFETCH(x, rw,loc)
+    #ifdef HAVE__MM_PREFETCH
+        /* _MM_HINT_ET[01] (rw = 1) unsupported, only available in gcc >= 4.9 */
+        #define CKDTREE_PREFETCH(x, rw, loc) _mm_prefetch((x), loc == 0 ? _MM_HINT_NTA : \
+                                                     (loc == 1 ? _MM_HINT_T2 : \
+                                                      (loc == 2 ? _MM_HINT_T1 : \
+                                                       (loc == 3 ? _MM_HINT_T0 : -1))))
+    #else
+        #define CKDTREE_PREFETCH(x, rw,loc)
+    #endif
 #endif
-#endif
-
 
 
 #define ckdtree_intp_t ssize_t
